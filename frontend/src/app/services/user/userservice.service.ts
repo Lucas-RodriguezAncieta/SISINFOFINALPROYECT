391d95849d8ef6e5d76f3262ed5312d9e551ee9f
AlanPerez <alan36755.yo@gmail.com>
2025-06-03 15:47:07 -0400

    servicios

--- frontend/src/app/services/user/userservice.service.ts
+++ frontend/src/app/services/user/userservice.service.ts
@@ -10,5 +10,23 @@ export class UserserviceService {
 
   constructor(private http:HttpClient) { }
 
-  
+  ObtainAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>('api/users');
+  }
+
+  ObtainUserById(id: number): Observable<User> {
+    return this.http.get<User>(`api/users/${id}`);
+  }
+
+  CreateUser(user: User): Observable<User> {
+    return this.http.post<User>('api/users', user);
+  }
+
+  UpdateUser(id: number, user: User): Observable<User> {
+    return this.http.put<User>(`api/users/${id}`, user);
+  }
+
+  DeleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`api/users/${id}`);
+  }
 }
