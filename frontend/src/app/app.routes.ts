f1464f0e6826b9ba0ad17c966fb29dd28297fdcb
AlanPerez <alan36755.yo@gmail.com>
2025-06-03 14:10:54 -0400

    a

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { RegistrarseComponent } from './pages/registrarse/registrarse.component'
 import { HistorialClinicoComponent } from './pages/historialclinico/historialclinico.component';
 
 export const routes: Routes = [
-  { path: 'inicio', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'recuperar-cuenta', component: RecuperarCuentaComponent },
   { path: 'registrarse', component: RegistrarseComponent },
   { path: 'historial', component: HistorialClinicoComponent },
-  { path: '', redirectTo:'/inicio', pathMatch: 'full'}
+
 ];
